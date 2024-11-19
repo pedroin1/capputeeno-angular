a15178ccdcb0650023c340111ff8e768b39f1b8b
Pedro Luiz <pedroluizpb45@outlook.com>
2024-11-19 01:25:04 -0300

    Criando icone do header e mixins flex pt.2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  itensCount: number = 2;
 }
